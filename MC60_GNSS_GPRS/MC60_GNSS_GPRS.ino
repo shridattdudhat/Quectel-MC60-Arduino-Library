8fcd7987ffac3f278d80d376e52d50ff778422cd
Shridatt Dudhat <dudhatshridatt@gmail.com>
2020-06-29 16:45:20 +0530

    Update MC60_GNSS_GPRS.ino

--- MC60_GNSS_GPRS/MC60_GNSS_GPRS.ino
+++ MC60_GNSS_GPRS/MC60_GNSS_GPRS.ino
@@ -1,23 +1,33 @@
+#include "OzHttpClient.h"
+#include <ArduinoJson.h>
 #define TINY_GSM_MODEM_MC60
-
 #include <TinyGsmClient.h>
-
 const char apn[]  = "ISAFE";
 const char user[] = "";
 const char pass[] = "";
 
-#include <SoftwareSerial.h>
-SoftwareSerial SerialAT(2, 3); // RX, TX
+//#include <SoftwareSerial.h>
+//SoftwareSerial SerialAT(2, 3); // RX, TX
 
-TinyGsm modem(SerialAT);
+#define SerialAT Serial3// RX, TX
+
+//TinyGsm modem(SerialAT);
+#include <StreamDebugger.h>
+StreamDebugger debugger(SerialAT, Serial);
+TinyGsm modem(debugger);
 
 TinyGsmClient client(modem);
+
+// Variable for http request
+OzHttpClient http;
+http_request_t request;
+http_response_t response;
 
 bool initializeGPS()
 {
 #ifdef TINY_GSM_MODEM_MC60
   Serial.println(F("Initializing GPS"));
-  modem.gpsON();
+  // modem.gpsON();
 #endif
 }
 
@@ -55,10 +65,10 @@ bool connectGPRS()
   Serial.print(apn);
 
   modem.gprsConnect(apn, user, pass);
-//  while (!modem.gprsConnect(apn, user, pass)) {
-//    Serial.println(" fail");
-//    delay(10000);
-//  }
+  while (!modem.gprsConnect(apn, user, pass)) {
+    Serial.println(" fail");
+    delay(10000);
+  }
   Serial.println(" OK");
   Serial.println(modem.getLocalIP());
   return true;
@@ -67,13 +77,45 @@ bool connectGPRS()
 void setup()
 {
   Serial.begin(9600);
-  SerialAT.begin(38400);
+  SerialAT.begin(9600);
   initializeGSM();
   connectGPRS();
+
+  delay(2000);
+
   initializeGPS();
 }
 
 void loop()
 {
+  bool status = false;
+
+  StaticJsonDocument<100> jsonBuffer;
+  JsonObject payload = jsonBuffer.to<JsonObject>();
+
+  payload["t"] = 34;
+  payload["lat"] = 23.56;
+  payload["lon"] = 73.77;
+
+  serializeJson(payload, Serial);
+  Serial.println("");
+
+  request.hostname = String("shridatt.eu-gb.mybluemix.net");
+  request.port = 80;
+  String path = "/gpsdata";
+  request.path = path;
+
+  serializeJson(payload, request.body);
+
+  http.post(client, request, response);
+
+  if (response.status == 200)
+  {
+    status = true;
+  }
+  else {
+    status = false;
+  }
+  return status;
 
 }
